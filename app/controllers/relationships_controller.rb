7087425064dda7f6762adac6aaf11fdbaa6fb023
shinjun14 <shinjunsakano@gmail.com>
2021-11-23 10:21:35 +0000

    非同期

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,27 @@
 class RelationshipsController < ApplicationController
+
+  #フォローする、フォロー外す
+  def create
+    current_user.follow(params[:user_id])
+    @user = User.find(params[:user_id])
+  end
+
+  def destroy
+    current_user.unfollow(params[:user_id])
+    @user =  User.find(params[:user_id])
+  end
+
+  #view
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
+    @osusume = Hobby.all.order(created_at: :desc).limit(3)
+  end
+
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
+    @osusume = Hobby.all.order(created_at: :desc).limit(3)
+  end
+
 end
